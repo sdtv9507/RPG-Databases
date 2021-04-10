7313aa01c8791879d3966ab258c5bb1c807a1a46
sdtv9507 <apr8950@gmail.com>
2021-04-10 19:02:37 -0400

    Finish method to add effects

--- addons/rpg_database/Scripts/Effects.cs
+++ addons/rpg_database/Scripts/Effects.cs
@@ -45,6 +45,60 @@ public class Effects : Container
         GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue1").Clear();
         GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue2").Clear();
     }
+
+    private void _on_ShowListCheckButton_pressed()
+    {
+        if (GetNode<CheckButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/CheckButton").Pressed == true)
+        {
+            GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/OptionButton").Disabled = false;
+        }
+        else
+        {
+            GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/OptionButton").Disabled = true;
+        }
+    }
+
+    private void _on_Value2CheckButton_pressed()
+    {
+        if (GetNode<CheckButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/CheckButton").Pressed == true)
+        {
+            GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/OptionButton").Disabled = false;
+        }
+        else
+        {
+            GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/OptionButton").Disabled = true;
+        }
+    }
+
+    private void _on_AddEffectConfirm_pressed()
+    {
+        String name = GetNode<LineEdit>("Tabs/Effects/AddEffect/VBoxContainer/Name/LineEdit").Text;
+        int selected = 0;
+        String dataType = "";
+        if (GetNode<CheckButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/CheckButton").Disabled == false)
+        {
+            selected = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/OptionButton").Selected;
+            dataType = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/ShowList/OptionButton").GetItemText(selected);
+        }
+        selected = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value1/OptionButton").Selected;
+        String value1 = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value1/OptionButton").GetItemText(selected);
+        String value2 = "";
+        if (GetNode<CheckButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/CheckButton").Disabled == false)
+        {
+            selected = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/OptionButton").Selected;
+            value2 = GetNode<OptionButton>("Tabs/Effects/AddEffect/VBoxContainer/Value2/OptionButton").GetItemText(selected);
+        }
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectNames").AddItem(name);
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/DataTypes").AddItem(dataType);
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue1").AddItem(value1);
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue2").AddItem(value2);
+        GetNode<WindowDialog>("AddEffect").Hide();
+    }
+
+    private void _on_AddEffectCancel_pressed()
+    {
+        GetNode<WindowDialog>("AddEffect").Hide();
+    }
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     //  public override void _Process(float delta)
     //  {
