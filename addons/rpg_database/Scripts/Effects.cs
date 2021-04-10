0d34aeef3666d13f76a6a6be5cb3445a727a07cd
sdtv9507 <apr8950@gmail.com>
2021-04-10 18:11:39 -0400

    Add Effect list's buttons methods

--- addons/rpg_database/Scripts/Effects.cs
+++ addons/rpg_database/Scripts/Effects.cs
@@ -7,15 +7,46 @@ public class Effects : Container
     // private int a = 2;
     // private string b = "text";
 
+    bool addNewEffect = true;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void _on_AddEffect_pressed()
+    {
+        addNewEffect = true;
+        GetNode<WindowDialog>("AddEffect").PopupCentered();
+    }
+
+    public void _on_EditEffect_pressed()
+    {
+        if (GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectNames").GetSelectedItems()[0] > -1)
+        {
+            addNewEffect = false;
+            GetNode<WindowDialog>("AddEffect").PopupCentered();
+        }
+    }
+
+    private void _on_RemoveEffect_pressed()
+    {
+        int selectedEffect = GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectNames").GetSelectedItems()[0];
+        if (selectedEffect > -1)
+        {
+            GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectNames").RemoveItem(selectedEffect);
+        }
+    }
+
+    private void _on_ClearEffects_pressed()
+    {
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectNames").Clear();
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue1").Clear();
+        GetNode<ItemList>("Tabs/Effects/EffectLabel/PanelContainer/VBoxContainer/Effects/EffectValue2").Clear();
+    }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 }
