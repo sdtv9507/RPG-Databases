88004ecf01f8468884ecfba64c8b14e7a8394bcc
sdtv9507 <apr8950@gmail.com>
2021-04-22 16:35:38 -0400

    Generate databases with gdscript

--- addons/rpg_databases_gd/database.gd
+++ addons/rpg_databases_gd/database.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 var instanced_scene: Control = null
 var button: Button = Button.new()
 
-func _enter_tree():
+func _enter_tree() -> void:
 	instanced_scene = load("res://addons/rpg_databases_gd/Scenes/Base.tscn").instance()
 	button.text = "Database"
 	button.connect("pressed", self, "_on_button_pressed")
@@ -12,14 +12,14 @@ func _enter_tree():
 	add_child(instanced_scene)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_child(instanced_scene)
 	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, button)
 	button.queue_free()
 	instanced_scene.queue_free()
 
-func _on_button_pressed():
-	if (instanced_scene.visible == true):
+func _on_button_pressed() -> void:
+	if (instanced_scene.visible == false):
 		var pos: Vector2 = Vector2(150, 150)
 		instanced_scene.set_position(pos)
 		instanced_scene.show()
